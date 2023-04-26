e955196a601ed3a6a3331ecbb0f6e10ea7774489
Andreas Löfkvist <andreas.lofkvist@GLNET.SE>
2023-04-26 21:07:50 +0200

    .

--- api/functions/authentication.py
+++ api/functions/authentication.py
@@ -1,9 +1,9 @@
 from functools import wraps
 from flask import request, abort
 import configparser, os
-from office365.runtime.auth.user_credential import UserCredential
-from office365.runtime.auth.client_credential import ClientCredential
-from office365.sharepoint.client_context import ClientContext 
+# from office365.runtime.auth.user_credential import UserCredential
+# from office365.runtime.auth.client_credential import ClientCredential
+# from office365.sharepoint.client_context import ClientContext 
 import urllib.parse
 import requests
 import json
@@ -22,95 +22,95 @@ def require_api_key(func):
 
 
 
-def get_sharepoint_context_using_user(site, username, password):
+# def get_sharepoint_context_using_user(site, username, password):
  
 
-    user_credentials = UserCredential(username, password)
+#     user_credentials = UserCredential(username, password)
 
-    # create client context object
-    ctx = ClientContext(site).with_credentials(user_credentials)
+#     # create client context object
+#     ctx = ClientContext(site).with_credentials(user_credentials)
 
-    return ctx
-def create_sharepoint_directory(ctx,dir_name):
-    """
-    Creates a folder in the sharepoint directory.
-    """
-    if dir_name:
-
-
-        result = ctx.web.folders.add(f'Shared Documents/{dir_name}').execute_query()
-
-        if result:
-            # documents is titled as Shared Documents for relative URL in SP
-            relative_url = f'Shared Documents/{dir_name}'
-            return relative_url
+#     return ctx
+# def create_sharepoint_directory(ctx,dir_name):
+#     """
+#     Creates a folder in the sharepoint directory.
+#     """
+#     if dir_name:
 
 
-def get_sharepoint_context_using_app(sharepoint_url):
+#         result = ctx.web.folders.add(f'Shared Documents/{dir_name}').execute_query()
 
-    # Initialize the client credentials
-    client_credentials = ClientCredential(config["SHAREPOINT"]["client_id"], config["SHAREPOINT"]["client_secret"])
+#         if result:
+#             # documents is titled as Shared Documents for relative URL in SP
+#             relative_url = f'Shared Documents/{dir_name}'
+#             return relative_url
 
-    # create client context object
-    ctx = ClientContext(sharepoint_url).with_credentials(client_credentials)
 
-    return ctx
+# def get_sharepoint_context_using_app(sharepoint_url):
 
-def get_sharepoint_access_headers_through_client_id():
-    client_id = config["SHAREPOINT"]["client_id"].strip()
-    client_secret = config["SHAREPOINT"]["client_secret"].strip()
-    tenant_id = config["SHAREPOINT"]["tenant_id"].strip()
-    tenant = config["SHAREPOINT"]["tenant"]
-    client_id = client_id + '@'+tenant_id
+#     # Initialize the client credentials
+#     client_credentials = ClientCredential(config["SHAREPOINT"]["client_id"], config["SHAREPOINT"]["client_secret"])
+
+#     # create client context object
+#     ctx = ClientContext(sharepoint_url).with_credentials(client_credentials)
+
+#     return ctx
+
+# def get_sharepoint_access_headers_through_client_id():
+#     client_id = config["SHAREPOINT"]["client_id"].strip()
+#     client_secret = config["SHAREPOINT"]["client_secret"].strip()
+#     tenant_id = config["SHAREPOINT"]["tenant_id"].strip()
+#     tenant = config["SHAREPOINT"]["tenant"]
+#     client_id = client_id + '@'+tenant_id
     
-    data = {
-    'grant_type':'client_credentials',
-    'resource': "00000003-0000-0ff1-ce00-000000000000/" + tenant + ".sharepoint.com@" + tenant_id, 
-    'client_id': client_id,
-    'client_secret': client_secret
-}
-    url = "https://accounts.accesscontrol.windows.net/tenant_id/tokens/OAuth/2"
-    headers = {
-    'Content-Type':'application/x-www-form-urlencoded'
-}
+#     data = {
+#     'grant_type':'client_credentials',
+#     'resource': "00000003-0000-0ff1-ce00-000000000000/" + tenant + ".sharepoint.com@" + tenant_id, 
+#     'client_id': client_id,
+#     'client_secret': client_secret
+# }
+#     url = "https://accounts.accesscontrol.windows.net/tenant_id/tokens/OAuth/2"
+#     headers = {
+#     'Content-Type':'application/x-www-form-urlencoded'
+# }
 
-    url = f"https://accounts.accesscontrol.windows.net/{tenant_id}/tokens/OAuth/2"
-    r = requests.post(url, data=data, headers=headers)
-    json_data = json.loads(r.text)
-    headers = {
-    'Authorization': "Bearer " + json_data['access_token'],
-    'Accept':'application/json;odata=verbose',
-    'Content-Type': 'application/json;odata=verbose'
-}
-    return headers
+#     url = f"https://accounts.accesscontrol.windows.net/{tenant_id}/tokens/OAuth/2"
+#     r = requests.post(url, data=data, headers=headers)
+#     json_data = json.loads(r.text)
+#     headers = {
+#     'Authorization': "Bearer " + json_data['access_token'],
+#     'Accept':'application/json;odata=verbose',
+#     'Content-Type': 'application/json;odata=verbose'
+# }
+#     return headers
 
-def create_sharepoint_directory(ctx,dir_name: str):
-    """
-    Creates a folder in the sharepoint directory.
-    """
-    if dir_name:
+# def create_sharepoint_directory(ctx,dir_name: str):
+#     """
+#     Creates a folder in the sharepoint directory.
+#     """
+#     if dir_name:
 
-        result = ctx.web.folders.add(f'Shared Documents/{dir_name}').execute_query()
+#         result = ctx.web.folders.add(f'Shared Documents/{dir_name}').execute_query()
 
-        if result:
-            # documents is titled as Shared Documents for relative URL in SP
-            relative_url = f'Shared Documents/{dir_name}'
-            return relative_url
+#         if result:
+#             # documents is titled as Shared Documents for relative URL in SP
+#             relative_url = f'Shared Documents/{dir_name}'
+#             return relative_url
 
-if __name__ == '__main__':
-    with open(os.path.join(os.path.dirname(__file__),'login.txt')) as f:
-        p= eval(f.read())
-    sitename = "TrdexperternaApplikationer"
-    URL =f"https://{tenant}.sharepoint.com/sites/{sitename}"
-    headers = get_sharepoint_access_headers_through_client_id()
-    url = URL+f"/_api/web/lists/getbytitle('Dagbok_poster')/fields"
-    l = requests.get(url, headers=headers)
-    #ctx = get_sharepoint_context_using_app(sitename)
-    #create_sharepoint_directory(ctx,'test directory')
-    url = URL+f"/_api/Web/GetFolderByServerRelativeUrl('/Shared Documents')/Files/Add(url='__init__.py', overwrite=true)"
-    url = URL+f"/_api/web/getfolderbyserverrelativeurl('')/Files/add(url='__init__.py', overwrite=true)"
-    payload=os.path.join(os.path.dirname(__file__),'Image_api.py')
-    with open(payload,'rb') as f:
-        response = requests.post(url, data=payload,headers=headers)
-        print(url)
-    print(response.text)
+# if __name__ == '__main__':
+#     with open(os.path.join(os.path.dirname(__file__),'login.txt')) as f:
+#         p= eval(f.read())
+#     sitename = "TrdexperternaApplikationer"
+#     URL =f"https://{tenant}.sharepoint.com/sites/{sitename}"
+#     headers = get_sharepoint_access_headers_through_client_id()
+#     url = URL+f"/_api/web/lists/getbytitle('Dagbok_poster')/fields"
+#     l = requests.get(url, headers=headers)
+#     #ctx = get_sharepoint_context_using_app(sitename)
+#     #create_sharepoint_directory(ctx,'test directory')
+#     url = URL+f"/_api/Web/GetFolderByServerRelativeUrl('/Shared Documents')/Files/Add(url='__init__.py', overwrite=true)"
+#     url = URL+f"/_api/web/getfolderbyserverrelativeurl('')/Files/add(url='__init__.py', overwrite=true)"
+#     payload=os.path.join(os.path.dirname(__file__),'Image_api.py')
+#     with open(payload,'rb') as f:
+#         response = requests.post(url, data=payload,headers=headers)
+#         print(url)
+#     print(response.text)
