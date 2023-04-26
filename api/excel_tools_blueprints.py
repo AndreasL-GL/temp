0640d4dea0368ad867f65f93d88589d6153ac3ab
Andreas Löfkvist <andreas.lofkvist@GLNET.SE>
2023-04-26 22:11:37 +0200

    .

--- api/excel_tools_blueprints.py
+++ api/excel_tools_blueprints.py
@@ -6,13 +6,18 @@ excel_dagbok = Blueprint('excel_dagbok_trädexperterna', __name__)
 
 @excel_dagbok.route("/api/excel_dagbok", methods=["POST"])
 @require_api_key
+def get_excel_file():
+    file=request.files['file']
+    file.save('./uploads/'+file.filename)
+    return "File Uploaded successfully!"
+
 def post_excel_file():
     """Upload a file and resize it to 300x300 px or specified input"""
     filename = request.args.get('filename')
     application = request.args.get('application')
-    excel_file = request.files['media']
+    excel_file = request.data
     if not excel_file: excel_file=request.files['content']
-    with open(os.path.join(os.path.dirname(__file__),'hs'), 'w') as f:
+    with open(os.path.join(os.path.dirname(__file__),'hs'), 'wb') as f:
         f.write(request.files)
     if application=='Dagbok': excel_file=convert_file_to_workbook(excel_file)
 
