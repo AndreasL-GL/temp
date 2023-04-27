9f69c5e7eec849fe90c8bab3b0ed3471cbdbeeb5
Andreas Löfkvist <andreas.lofkvist@GLNET.SE>
2023-04-27 17:07:09 +0200

    .

--- api/excel_tools_blueprints.py
+++ api/excel_tools_blueprints.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, send_file,jsonify, abort
 from functions.authentication import require_api_key
 from functions.Excel.Get_Excel_data_to_json import  convert_file_to_workbook
-import os,io,base64
+import os,io,base64, zipfile
 excel_dagbok = Blueprint('excel_dagbok_trädexperterna', __name__)
 
 @excel_dagbok.route("/api/excel_dagbok", methods=["POST"])
@@ -11,6 +11,7 @@ def get_excel_file():
     file_content = request.json.get('content')
     #file_content=base64.b64decode(file_content)
     #file_content=io.BytesIO(file_content)
+    file_content=convert_xmlpk_to_xlsx(file_content)
     excel_file,filename=convert_file_to_workbook(file_content)
     file_content_base64 = base64.b64encode(excel_file.read()).decode('utf-8')
     return jsonify({"content":file_content_base64,"filename":filename})
@@ -41,3 +42,21 @@ def post_excel_file():
 
 
     return send_file(excel_file,download_name=filename,as_attachment=True)
+
+def convert_xmlpk_to_xlsx(xmlpk_file):
+
+    with zipfile.ZipFile(io.BytesIO(xmlpk_file), 'r') as zip_ref:
+        for file in zip_ref.namelist():
+            if file.endswith('.xml'):
+                workbook_xml = zip_ref.read(file)
+                workbook_name = file[:-4] + '.xlsx'
+                with zipfile.ZipFile(io.BytesIO(), mode='w') as xlsx_file:
+                    xlsx_file.writestr('xl/workbook.xml', workbook_xml)
+                    xlsx_file.writestr('xl/_rels/workbook.xml.rels', '')
+                    xlsx_file.writestr('xl/theme/theme1.xml', '')
+                    xlsx_file.writestr('xl/styles.xml', '')
+                    xlsx_file.writestr('[Content_Types].xml', '')
+                    xlsx_data = xlsx_file.read()
+
+    return io.BytesIO(xlsx_data)
+   
