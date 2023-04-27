b2b996f8b93a9b122fadce2dbbd362de47adb8b0
Andreas Löfkvist <andreas.lofkvist@GLNET.SE>
2023-04-27 15:22:15 +0200

    .

--- api/image_api_blueprints.py
+++ api/image_api_blueprints.py
@@ -10,10 +10,12 @@ image_resize = Blueprint('image_resize', __name__)
 @require_api_key
 def post_file():
     file_content = request.json.get('content')
-    width = request.json.get('width')
-    height = request.json.get('height')
+    width = int(request.json.get('width'))
+    height = int(request.json.get('height'))
     file_content=base64.b64decode(file_content)
+    file_content=io.BytesIO(file_content)
     img_file=resize_and_autoorient(file_content, height,width)
+    
     file_content=base64.b64enchode(img_file.read()).decode('utf-8')
     return jsonify({"content":file_content})
     
