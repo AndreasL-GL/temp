93c66f48bfee07bb75f554f08eee847b23dccf08
Andreas Löfkvist <andreas.lofkvist@GLNET.SE>
2023-04-27 15:24:48 +0200

    .

--- api/image_api_blueprints.py
+++ api/image_api_blueprints.py
@@ -14,6 +14,7 @@ def post_file():
     height = int(request.json.get('height'))
     file_content=base64.b64decode(file_content)
     file_content=io.BytesIO(file_content)
+    print(file_content)
     img_file=resize_and_autoorient(file_content, height,width)
     
     file_content=base64.b64enchode(img_file.read()).decode('utf-8')
