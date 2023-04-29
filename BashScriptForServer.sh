c0701710e80d0e791f5dfc2d58422991f45c29b9
Andreas Löfkvist <andreas.lofkvist@GLNET.SE>
2023-04-29 14:47:20 +0200

    .

--- BashScriptForServer.sh
+++ BashScriptForServer.sh
@@ -3,7 +3,7 @@ if [ ! -d ~/items/trädgårdsexperterna ]; then
   mkdir -p ~/items/trädgårdsexperterna
 fi
 container_id=$(sudo docker container ls -f name=api -q)
-sudo docker cp -r $container_id:/usr/src/app/functions/Excel/items_trädgårdsexperterna ~/items/trädgårdsexperterna
+sudo docker container cp -r $container_id:/usr/src/app/functions/Excel/items_trädexperterna/ ~/items/trädexperterna/.
 sudo docker compose down
 sudo rm -r temp
 git clone https://github.com/AndreasL-GL/temp.git
