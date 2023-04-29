1392dfbf0666affe804b659b4546ef5bee3c45ff
Andreas Löfkvist <andreas.lofkvist@GLNET.SE>
2023-04-29 14:39:47 +0200

    .

--- BashScriptForServer.sh
+++ BashScriptForServer.sh
@@ -4,9 +4,8 @@ if [ ! -d ~/items/trädgårdsexperterna ]; then
 fi
 container_id=$(sudo docker container ls -f name=api -q)
 sudo docker cp $container_id:/usr/src/app/functions/Excel/items_trädgårdsexperterna ~/items/trädgårdsexperterna
-
+sudo docker compose down
 sudo rm -r temp
 git clone https://github.com/AndreasL-GL/temp.git
 cd temp
-cd api
-sudo python3 app.py
+sudo docker compose up --build -d
